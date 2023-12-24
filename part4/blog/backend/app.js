a6fc5ae7c943f89347ec6fb4442fcf0155a2e64a
Chamu Mutezva <ckmutezva@gmail.com>
2023-12-24 09:04:49 +0200

    adding middleware to part4 blog

--- part4/blog/backend/app.js
+++ part4/blog/backend/app.js
@@ -1,22 +1,26 @@
 const config = require("./utils/config");
+const middleware = require("./utils/middleware")
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blog");
+const logger = require("./utils/logger");
 
 mongoose.set("strictQuery", false);
 
 mongoose
     .connect(config.MONGO_URI)
     .then(() => {
-        console.log("connected to MongoDB");
+        logger.info("connected to MongoDB");
     })
     .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message);
+        logger.error("error connecting to MongoDB:", error.message);
     });
 
 app.use(cors());
 app.use(express.json());
-app.use(blogRouter)
+app.use(blogRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 module.exports = app;
